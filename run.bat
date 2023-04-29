ef300d5e598485973d5c3ee9dfcaaa373b8a1d08
David Aguilera <powdavyd@gmail.com>
2023-04-29 18:57:45 +0200

    hola

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+git pull
 REM Forge requires a configured set of both JVM and program arguments.
 REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
